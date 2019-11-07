a1d40680ced4112dfd87f32a83aeb80c1ef79149
Filipe Tristão <filipedtristao@gmail.com>
2019-11-07 16:58:09 -0300

    Format MissingIdException

--- src/Exceptions/MissingIdException.php
+++ src/Exceptions/MissingIdException.php
@@ -1,15 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Filipe
- * Date: 06/11/2019
- * Time: 12:57
- */
-
 namespace CrudBuilder\Exceptions;
-
 
 class MissingIdException extends InvalidRequestException
 {
-
 }
