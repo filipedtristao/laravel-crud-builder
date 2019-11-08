4ff657b96bf74dca99307e9c998afc71d9f90fd0
Filipe Tristão <filipedtristao@gmail.com>
2019-11-08 11:44:34 -0300

    Helpers methods to ignore softdelete and timestamps attributes on request

--- src/Concerns/AddsIgnoreAttributesToModel.php
+++ src/Concerns/AddsIgnoreAttributesToModel.php
@@ -19,10 +19,35 @@ trait AddsIgnoreAttributesToModel
             throw MethodCallException::mustBeCalledBefore('ignoreAttributes', 'allowedAttributes');
         }
 
+        $this->initIgnoreAttributes();
+
         $attributes = is_array($attributes) ? $attributes : func_get_args();
-        $this->ignoreAttributes = collect($attributes);
+        $this->ignoreAttributes = $this->ignoreAttributes->merge(collect($attributes));
 
         return $this;
     }
+
+    public function ignoreSoftDelete(): self
+    {
+        $this->initIgnoreAttributes();
+        $this->ignoreAttributes = $this->ignoreAttributes->merge(['deleted_at']);
+
+        return $this;
+    }
+
+    public function ignoreTimestamps(): self
+    {
+        $this->initIgnoreAttributes();
+        $this->ignoreAttributes = $this->ignoreAttributes->merge(['created_at', 'updated_at']);
+        return $this;
+    }
+
+    protected function initIgnoreAttributes()
+    {
+        if (!$this->ignoreAttributes) {
+            $this->ignoreAttributes= collect([]);
+        }
+    }
+
 
 }
